892c5342286603cd8d2eab7674ff6ca2e1a99c7c
dexterleslie@gmail.com <dexterleslie@gmail.com>
2020-07-26 19:53:59 +0800

    Add new file learn-if.py

---
+++ learn-if.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+# -*- coding: UTF-8 -*-
+
+var_name = "Dexter"
+if var_name == "Dexter" :
+ print("Hello Dexter")
+else :
+ print("Hello anonymous")
+
+var_num = 3
+if var_num == 1 :
+ print("var_num=1")
+elif var_num == 2 :
+ print("var_num=2")
+elif var_num == 3 :
+ print("var_num=3")
+else :
+ print("var_num=4")
+
+var_num = 9
+if var_num>=0 and var_num<=10 :
+ print("var_num is between 0 to 10")
